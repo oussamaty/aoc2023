3702a35a13f9107a274a1d3b4c90fe82e2651cce
Oussama Janah Eddine <oussamaty62@gmail.com>
2023-12-14 03:16:46 +0100

    Day 11

---
+++ Day11/d11p2.py
@@ -0,0 +1,31 @@
+f = open('Day11/input.txt')
+l = f.read().split('\n')
+
+pos = []
+mark = [set(), set()]
+
+for i in range(len(l)):
+    for j in range(len(l[i])):
+        if l[i][j] == "#":
+            pos.append((i, j))
+            mark[0].add(i)
+            mark[1].add(j)
+
+ind = [[0]*len(l), [0]*len(l[0])]
+for i in range(1, len(l)):
+    if i in mark[0]:
+        ind[0][i] = ind[0][i - 1] + 1
+    else:
+        ind[0][i] = ind[0][i - 1] + 1000000
+for i in range(1, len(l[0])):
+    if i in mark[1]:
+        ind[1][i] = ind[1][i - 1] + 1
+    else:
+        ind[1][i] = ind[1][i - 1] + 1000000
+
+result = 0
+for i in range(len(pos)):
+    for j in range(i + 1, len(pos)):
+        result += abs(ind[0][pos[j][0]] - ind[0][pos[i][0]]) + abs(ind[1][pos[j][1]] - ind[1][pos[i][1]])
+
+print(result)
